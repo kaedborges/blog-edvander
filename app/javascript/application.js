9864049b94404588808002e3e13c4764a5e8a24c
Edvander Borges <edvander.borges@nextbss.co.ao>
2022-12-26 22:20:04 +0100

    Add tooltip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import 'bootstrap'
+
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
